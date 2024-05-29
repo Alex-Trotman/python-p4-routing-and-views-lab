3d97546d5321ded9bde32602ac3d18568ec7dc7a
Alex <alextrotman04@gmail.com>
2024-05-29 13:25:53 -0500

    save

--- server/app.py
+++ server/app.py
@@ -6,3 +6,19 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:string>')
+def print_string(string):
+    print(string)
+    return string
+
+@app.route('/count/<int:num>')
+def count(num):
+    numbers = range(0, num)
+    result = '\n'.join(str(n) for n in numbers)
+    return result + '\n'
+    
