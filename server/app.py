ab1e4aec1121109a76d4172de4f051b54a750faa
Alex Trotman <alextrotman04@gmail.com>
2024-05-29 19:15:41 -0500

    finished

--- server/app.py
+++ server/app.py
@@ -22,3 +22,19 @@ def count(num):
     result = '\n'.join(str(n) for n in numbers)
     return result + '\n'
     
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = 0
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+
+    return str(result)
